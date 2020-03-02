dcc1b7b8faa4395601fb0599255ac0ad377a64df
kevinloustau <kevinloustau@users.noreply.github.com>
2020-03-02 23:27:24 +0100

    add ejs module

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
 
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+// server
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', e => console.error(e))
@@ -11,7 +19,10 @@ db.once('open', e => console.log('Connected to db'))
 
 app.use(express.json())
 
+const indexRouter = require('./routes/index')
+app.use('/', indexRouter)
+
 const quotesRouter = require('./routes/quotes')
 app.use('/quotes', quotesRouter)
 
-app.listen(3000, () => console.log('Started'))
+app.listen(process.env.PORT || 3000, () => console.log('Started'))
